fc71a8a8e789714b3bcf7c15a53cab2a1f89fa9d
Barbaragc <barbara_g_96@hotmail.com>
2017-09-12 11:14:47 +0200

    [BGC] Clean Code

--- javascript.js
+++ javascript.js
@@ -1,20 +1,18 @@
 var cont=0;
 
-function initEvents(){
-	$("td").click(function(e){
-		
-		
+function initEvents()
+{
+	$("td").click(function(e)
+	{
 		var but = $(this);
-	
 		var player = turn%2==0 ? "equis" : "circulo";
-		
 		$(but).children().addClass(player);
 		turn++;
-		console.log($(but).children())
-	
+		console.log($(but).children());
 	});
 	
-	$("#limpiaButton").click(function(){
+	$("#limpiaButton").click(function()
+	{
 		$("td div").removeClass("circulo");
 		$("td div").removeClass("equis");
 		$("td div").addClass("button");
@@ -23,36 +21,35 @@ function initEvents(){
 }
 
 function crear(numRow, numCol)
+{
+	for(var a=0; a<numRow; a++)
+	{
+		$("table").append("<tr id='f"+a+"'></tr>");
+		for(var b=10; b<13; b++)
 		{
-			for(var a=0; a<numRow; a++)
+			$("#f"+a).append("<td id='f"+a+"c"+b+"'><div class='lifes'></div> </td>");
+			console.log($("#f"+a+"c"+b+""));
+		}
+	}
+
+	for(var a=0; a<numRow; a++)
+	{
+		for(var b=10; b<numCol; b++)
+		{
+			if(a!=0)
 			{
-				$("table").append("<tr id='f"+a+"'></tr>");
-				for(var b=10; b<13; b++)
-				{
-					$("#f"+a).append("<td id='f"+a+"c"+b+"'><div class='lifes'></div> </td>");
-					console.log($("#f"+a+"c"+b+""));
-				}
+				var aa=a--;
+				if( ($("#f"+aa+"c"+b+"")) )
+			
 			}
-		
-			for(var a=0; a<numRow; a++)
-			{
-				for(var b=10; b<numCol; b++)
-				{
-					if(a!=0)
-					{
-						var aa=a--;
-						if( ($("#f"+aa+"c"+b+"")) )
-					
-					}
-				}
-			}
-		} //Final Crear
+		}
+	}
+} //Final Crear
 
 $(document).ready(function()
 {
 	crear(numRow, numCol);
 	var turn = 0;
-	
 	initEvents();
 	
 });
